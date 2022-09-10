849482b62e92a4fb4faeac12cc2adeda1f16f62a
cade lueker <cadelueker@cades-MacBook-Air.local>
2022-09-10 12:39:52 -0600

    literate config

--- config.el
+++ config.el
@@ -1,7 +1,3 @@
-;; -----------------------------
-;; ----- Config / Packages -----
-;; -----------------------------
-
 ;; ----- Native Comp Settings
 (setq warning-minimum-level :error)
 
@@ -96,7 +92,7 @@
       (org-roam-db-sync)
       (org-id-update-id-locations)
       (org-roam-update-org-id-locations)))
-  
+
 ;; ----- Rainbow-delimiters
 (use-package rainbow-delimiters
   :ensure t)
@@ -137,8 +133,8 @@
 ;; line numbers
 (global-display-line-numbers-mode)
 (add-hook 'org-mode-hook
-	  (lambda ()
-	    (display-line-numbers-mode -1)))
+          (lambda ()
+            (display-line-numbers-mode -1)))
 
 ;; ----- TreeSitter
 (use-package tree-sitter
@@ -152,8 +148,207 @@
   :ensure t)
 (setq vterm-kill-buffer-on-exit t)
 (add-hook 'vterm-mode-hook
-	  (lambda ()
-	    (display-line-numbers-mode -1)))
+          (lambda ()
+            (display-line-numbers-mode -1)))
 
-(provide 'config)
-;;; config.el ends here
+;; ----- LSP -----
+;; ----- lsp-mode
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((rust-mode . lsp)
+         (js-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; Flycheck
+(use-package flycheck
+  :ensure t
+  :config
+  (global-flycheck-mode))		; global enable
+;; use flycheck by default
+(setq flycheck-standard-error-navigation t)
+
+;; Lsp UI
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+(setq lsp-ui-sideline-show-diagnostics t)
+
+;; Helm
+(use-package helm
+  :ensure t)
+;; remap chords to helm
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)				; global enable
+(use-package helm-lsp
+  :ensure t
+  :commands helm-lsp-workspace-symbol)
+
+;; ----- Company
+(use-package company
+  :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
+;; ---------------
+
+;; ----- Go
+(use-package go-mode
+  :ensure t)
+;; lang specific hooks
+(add-hook 'go-mode-hook #'tree-sitter-hl-mode)
+(add-hook 'go-mode-hook #'smartparens-mode)
+
+;; ----- JS
+;; lang specific hooks
+(add-hook 'js-mode-hook #'tree-sitter-hl-mode)
+(add-hook 'js-mode-hook #'smartparens-mode)
+;; run org blocks
+(require 'ob-js)
+
+;; ----- Rust
+(use-package rust-mode
+  :ensure t)
+;; org src support
+(use-package ob-rust
+  :ensure t)
+
+;; lang specific hooks
+(add-hook 'rust-mode-hook #'tree-sitter-hl-mode)
+(add-hook 'rust-mode-hook #'smartparens-mode)
+
+;; ----- Zig
+(use-package zig-mode
+  :ensure t)
+
+;; ----- Org Config
+;; better indentation
+(add-hook 'org-mode-hook
+          'org-indent-mode)
+;; load babel languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((js . t)
+   (rust . t)))
+
+;; ---------------------
+;; ----- Functions -----
+;; ---------------------
+
+(defun custom-js-format-buffer ()
+  "Formats a js buffer using the deno formatter."
+  (interactive)
+  (let ((xfpath (buffer-file-name)))
+    (if xfpath
+        (progn
+          (save-buffer)
+          (shell-command
+           (format "deno fmt %s"
+                   (shell-quote-argument xfpath)))
+          (revert-buffer t t t))
+      (user-error "Current buffer must be a file"))))
+
+;; ----- All Modes 
+(general-define-key
+ :states 'normal
+ :prefix "SPC"
+ ;; buffer management
+ "b s" '(switch-to-buffer :which-key "switch to buffer")
+ "b k" '(kill-buffer-and-window :which-key "kill buffer and window")
+ "b K" '(kill-some-buffers :which-key "kill some buffers")
+ ;; compiling
+ "c c" '(compile :which-key "compile")
+ ;; Helm 
+ "." '(helm-find-files :which-key "helm-find-files")
+ ;; Neotree
+ "n" '(neotree-toggle :which-key "neotree toggle")
+ ;; Org Roam
+ "r f" '(org-roam-node-find :which-key "org roam node find"))
+
+;; ----- Evil Bindings
+;; Yanking to end of line
+(general-define-key
+ :states 'normal
+ "Y" (kbd "y$"))
+
+;; ----- Center Searching 
+;; search next
+(defun search-next-center-evil ()
+  (interactive)
+    (evil-search-next)
+    (evil-scroll-line-to-center
+      (line-number-at-pos (point))))
+;; search previous 
+(defun search-prev-center-evil () 
+  (interactive)
+    (evil-search-previous)
+    (evil-scroll-line-to-center
+      (line-number-at-pos (point))))
+;; remap
+(general-define-key
+ :states 'normal
+ "n" 'search-next-center-evil)
+(general-define-key
+ :states 'normal
+ "N" 'search-prev-center-evil)
+;; ---------------------- 
+
+;; ----- LSP
+;; nvim like bindings
+(general-define-key
+ :states 'normal
+ :keymaps 'lsp-ui-mode-map
+ ;; hover
+ "K" '(lsp-ui-doc-show :which-key "lsp ui doc show")
+ ;; finding def / references
+ "gd" '(lsp-ui-peek-find-definitions :which-key "lsp ui peak find definitions")
+ "gr" '(lsp-ui-peek-find-references :which-key "lsp ui peak find references"))
+;; formatting 
+(general-define-key
+ :states '(normal visual)
+ :keymaps 'lsp-mode-map
+ :prefix "SPC"
+ "f b" '(lsp-format-buffer :which-key "lsp format buffer")
+ "f r" '(lsp-format-region :which-key "lsp format region"))
+
+;; ----- Neotree
+;; evil bindings
+(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+(evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+(evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+(evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+(evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+(evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+
+;; ----- JS  
+(general-define-key
+ :states 'normal
+ :keymaps 'js-mode-map
+ :prefix "TAB"
+ "f" '(custom-js-format-buffer :which-key "custom-js-format-buffer"))
+
+;; ----- Org Roam
+(general-define-key
+ :states 'normal
+ :keymaps 'org-mode-map
+ :prefix "SPC"
+ "r i" '(org-roam-node-insert :which-key "org roam node insert")
+ "r b" '(org-roam-buffer-toggle :which-key "org roam buffer toggle"))
+
+;; ----- Smartparens
+;; ()
+(sp-pair "(" ")" :wrap "C-(")
+;; {}
+(sp-pair "{" "}" :wrap "C-{")
+;; []
+(sp-pair "[" "]" :wrap "M-[")
+
+;; ----- V Term
+(general-define-key
+ :states 'normal
+ :prefix "SPC"
+ "v" '(vterm-other-window :which-key "vterm-other-window"))
--- init.el
+++ init.el
@@ -1,9 +1,6 @@
-;; ----- Config / Packages 
-(load-file (expand-file-name "config.el" user-emacs-directory))
-;; ----- Languages
-(load-file (expand-file-name "languages.el" user-emacs-directory))
-;; ----- Keybindings
-(load-file (expand-file-name "keybindings.el" user-emacs-directory))
+;; Load Literate Config File 
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -14,7 +11,7 @@
    '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "b4e786d88aeb48bce6c3b93a72d50e9c98966b759b2b09d837ea93e25acb8cc2" "0ed28b0694dd2c7a2407598e63650a8562b9e833a1a136ee74790a74d3776d3b" default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(solarized-theme monokai-theme monokai evil-collection gruvbox-theme rainbow-delimiters smartparens ob-rust flycheck el evil undo-fu which-key vterm use-package general exec-path-from-shell)))
+   '(magit git-gutter solarized-theme monokai-theme monokai evil-collection gruvbox-theme rainbow-delimiters smartparens ob-rust flycheck el evil undo-fu which-key vterm use-package general exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
